1bbe7b869f92ba76256c2793cb50e37ee3a1bb87
acesfullblown <dave.southard@protonmail.com>
2024-07-27 21:13:24 -0400

    Add CandyStoreAPI solution

---
+++ CandyStoreAPI/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using CandyStoreAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CandyStoreAPI.Models;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+}
